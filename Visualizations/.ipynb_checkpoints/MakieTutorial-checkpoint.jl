addc4a3de9269310036650e82cec71159aef7769
Davi Barreira <davibarreira@pop-os.localdomain>
2022-09-12 20:58:37 -0300

    Enviando pos problemas com ssd.

--- Visualizations/.ipynb_checkpoints/MakieTutorial-checkpoint.jl
+++
@@ -1,7 +0,0 @@
-# Let's cover some basics of Makie
-#  
-using CairoMakie
-using AlgebraOfGraphics
-kwargs = AlgebraOfGraphics.aog_theme()
-set_theme!(;resolution=(500, 500),kwargs...)
-fig, ax, pltobj = scatterlines(1:10);
